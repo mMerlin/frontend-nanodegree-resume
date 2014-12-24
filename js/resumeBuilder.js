8227142e4bc52722c7d88af8e9d93a94e2b7f9df
H. Phil Duby <philduby@phriendly.net>
2014-12-24 16:39:40 -0700

    Build header using *mail merge* from strings
    UDACITY > Javascript Basic > Lesson 1 - Data Types >
    Resume Header Quiz

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,13 @@
-var awesomeThoughts = 'I am Phil and I am AWESOME!';
-var funThoughts = awesomeThoughts.replace('AWESOME', 'FUN');
-$('#main').append(funThoughts);
+var placeholderText = '%data%'; //common search string
+var me = {};//empty object; reduce namespace pollution
+me.name = 'H. Phil Duby';
+me.role = 'Web Developer';
+//resumeHTML = {};
+//ResumeHTML.headerName = '{stuff}%data%{stuff}';
+//var formatedResume = {};
+//formatedResume.Name = ResumeHTML.header.name.replace(placeholderText, me.name);
+var formattedName = HTMLheaderName.replace(placeholderText, me.name);
+var formattedRole = HTMLheaderRole.replace(placeholderText, me.role);
+
+$('#header').prepend(formattedName);
+$('#header').append(formattedRole);
