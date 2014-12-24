bd74b2a5259e92957ca3686626943be996e70430
H. Phil Duby <philduby@phriendly.net>
2014-12-24 16:30:43 -0700

    append modified text to page
    UDACITY > Javascript Basic > Lesson 1 - Data Types >
    string.replace() Quiz

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,3 @@
-var email = "cameron@udacity.com";
-var newEmail = email.replace("udacity", "gmail");
-console.log(email);
-console.log(newEmail);
+var awesomeThoughts = 'I am Phil and I am AWESOME!';
+var funThoughts = awesomeThoughts.replace('AWESOME', 'FUN');
+$('#main').append(funThoughts);
